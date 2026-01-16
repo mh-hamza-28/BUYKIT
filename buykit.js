cf366f3fa9f7b51ca3547ad1ca929afbc49c0c52
hamza-28-06 <txtpc3182811@gmail.com>
2026-01-16 19:02:12 +0530

    Stage1-SDLC( PLANNIND AND DESIGNING)(initially) layout and design done. html and css and generated html with javascript for interactivity.

--- buykit.js
+++ buykit.js
@@ -1,24 +1,33 @@
 let buykit='';
+products.forEach((product) => {
+    // Determine badge class
+    let badgeClass = '';
+    if (product.badge==='BESTSELLER'){
+        badgeClass='bestseller'
+    }
 
-products.forEach((product)=>{
-    buykit+=`
-      <div class="jersey-card grid">
-                <div class="card-badge">${product.badge}</div>
-                <div class="jersey-image">
-    <div class="jersey-card card-gold">
-     <div class="jersey-image">
-     <img src="${product.image}"alt="${product.team}" style="width:100%; max-width:220px;">
-      </div>
-
-      <div class="card-content">
-               <span class="team-league">${product.league}</span>
-                    <h3>${product.team}</h3>
-                    <p class="jersey-desc">${product.season}</p>
-                    <div class="price-row">
-                        <span class="price">$${((product.price)/100).toFixed(2)}</span>
-                    </div>
-                    <button class="button button-primary">Add to Cart</button>
+    else if (product.badge === 'NEW') {
+        badgeClass = 'new';
+    } else if (product.badge === 'HOT') {
+        badgeClass = 'hot';
+    }
+ 
+    
+ buykit += `
+        <div class="jersey-card">
+            <div class="card-badge ${badgeClass}">${product.badge}</div>
+            <div class="jersey-image">
+                <img src="${product.image}" alt="${product.team}" style="width:100%; max-width:220px;">
+            </div>
+            <div class="card-content">
+                <span class="team-league">${product.league}</span>
+                <h3>${product.team}</h3>
+                <p class="jersey-desc">${product.season}</p>
+                    <span class="price"><u>$${(product.price / 100).toFixed(2)}</u></span>
                 </div>
-            </div>`;
+                <button class="button button-primary">Add to Cart</button>
+            </div>
+        </div>`;
 });
-document.querySelector('.grid').innerHTML=buykit;
+
+document.querySelector('#jersey-grid').innerHTML = buykit;
---
+++ product.js
@@ -0,0 +1,146 @@
+const products = [
+    {
+        image: "images/jersey/r-madrid.png",
+        team: "REAL MADRID",
+        price: 8420,
+        league: "LA LIGA",
+        season: "2025/26",
+        badge: "BESTSELLER"
+    },
+    {
+        image: "images/jersey/barca.png",
+        team: "FC BARCELONA",
+        price: 8499,
+        league: "LA LIGA",
+        season: "2025/26",
+        badge: "NEW"
+    },
+    {
+        image: "images/jersey/amadrid2004.png",
+        team:"ATLÉTICO MADRID 2004/05",
+        price: 10420,
+        league: "LA LIGA",
+        season: "2004/05",
+        badge: "HOT"
+    },
+    {
+        image: "images/jersey/manu.png",
+        team: "MANCHESTER UNITED",
+        price: 8999,
+        league: "PREMIER LEAGUE",
+        season: "2025/26",
+        badge: "NEW"
+    },
+    {
+        image: "images/jersey/arsenal.png",
+        team: "ARSENAL FC - 3rd KIT",
+        price: 8750,
+        league: "PREMIER LEAGUE",
+        season: "2025/26",
+        badge: "BESTSELLER"
+    },
+    {
+        image: "images/jersey/mancity.png",
+        team: "MANCHESTER CITY - AWAY",
+        price: 8950,
+        league: "SERIE A",
+        season: "2025/26",
+        badge: "BESTSELLER"
+    },
+    {
+        image: "images/jersey/milan125.png",
+        team: "AC MILAN - 125th ANNIVERSARY",
+        price: 10600,
+        league: "SERIE A",
+        season: "2025/26",
+        badge: "HOT"
+    },
+    {
+        image: "images/jersey/inter.png",
+        team: "INTERNAZIONALE MILAN",
+        price: 10200,
+        league: "SERIE A",
+        season: "2004/05",
+        badge: "HOT"
+    },
+    {  image: "images/jersey/JUVENTUS.png",
+        team: "JUVENTUS - AWAY",
+        price: 10200,
+        league: "SERIE A",
+        season: "2020/21",
+        badge: "HOT"
+    },
+    {
+        image: "images/jersey/BAYERN.png",
+        team: "FC BAYERN MUNICH - AWAY",
+        price: 8600,
+        league: "BUNDESLIGA",
+        season: "2025/26",
+        badge: "NEW",
+        
+    },
+    {
+        image: "images/jersey/bvb.png",
+        team: "BORUSSIA DORTMUND",
+        price: 8817,
+        league: "BUNDESLIGA",
+        season: "2025/26",
+        badge: "NEW"
+    },
+    {
+        image: "images/jersey/lvksen.png",
+        team: "LEVERKUSEN",
+        price: 8817,
+        league: "BUNDESLIGA",
+        season: "2025/26",
+        badge: "NEW"
+    },  
+        { 
+        image: "images/jersey/miami.png",
+        team: "INTER MIAMI FC - AWAY",
+        price: 8817,
+        league: "MLS",
+        season: "2025/26",
+        badge: "NEW"
+        },  
+        {
+        image: "images/jersey/LAGAL.png",
+        team: "LA GALAXY FC ",
+        price: 8817,
+        league: "MLS",
+        season: "2025/26",
+        badge:"HOT"
+        },
+        {
+        image: "images/jersey/PHP.png",
+        team: "PHILADELPHIA UNION ",
+        price: 8817,
+        league: "MLS",
+        season: "2025/26",
+        badge: "NEW"
+        },
+        {
+        image: "images/jersey/alnasr.png",
+        team: " AL NASSR FC ",
+        price: 8817,
+        league: "SAUDI PRO LEAGUE",
+        season: "2025/26",
+        badge:"HOT"
+        },
+        {
+        image: "images/jersey/HILAL.png",
+        team: "AL HILAL FC",
+        price: 8817,
+        league: "SAUDI PRO LEAGUE",
+        season: "2025/26",
+        badge:"BESTSELLER"
+        },
+        {
+          image: "images/jersey/ITTI.png",
+        team: "AL ITTIHAD FC",
+        price: 8817,
+        league: "SAUDI PRO LEAGUE",
+        season: "2025/26",
+        badge:"NEW"
+        }
+];
